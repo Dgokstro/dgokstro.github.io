31375113c39f9fe44a7d072ea68b900e308aedc7
Diogo Castro <diogo.campos.castro@gmail.com>
2024-05-27 23:24:02 -0300

    fix: add public dir

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base:'/dgokstro.github.io/',
+  publicDir:'./dist',
   plugins: [react()],
 })
